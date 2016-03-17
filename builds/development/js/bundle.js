bfe60e35aca5d0be27f5578925aa71c8068674bc
J Pazmino <jpazmino2003@gmail.com>
2016-03-17 15:07:48 -0400

    working show all by site

--- builds/development/js/bundle.js
+++ builds/development/js/bundle.js
@@ -1,78 +1,108 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var Firebase = require('firebase');
+// bysite
+(function() {
 
-var rootRef = new Firebase('https://scrap-dev-news.firebaseio.com/');
 
-rootRef.child("settings").once("value", function(snapshot) {
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var date = new Date(snapshot.val().lastupdate);
-    var formattedDate = date.toDateString();
-    var formattedTime = date.toTimeString();
-    var updateField = document.getElementById("updatetime");
-    updateField.innerHTML = formattedDate.slice(4, 10) + " @ " + formattedTime.slice(0, 5);
-});
+    var Firebase = require('firebase');
 
-//all
-rootRef.child("articles").once("value").then(function(snapshot) {
-    var snapshotArray = [];
-    snapshot.forEach(function(snap) {
-        snapshotArray.push(snap.val());
-    });
-    var articles = snapshotArray.reverse(); //latest articles show up first
-    
-    for (var key in articles) {
-        if (articles.hasOwnProperty(key)) {
-            var articleData = articles[key];
-            var siteTag = articleData.tag;
-            
-            var article = document.createElement("article");
-            article.innerHTML = '<svg class="left" width="54" height="54" viewbox="0 0 200 200"><use xlink:href="#sym-' + siteTag + '"></use></svg>';
-            
-            var title = document.createElement("a");
-            title.className = "title";
-            title.innerHTML = articleData.title;
-            title.setAttribute("href", articleData.url);
-            title.setAttribute("target", "_blank");
-            
-            var time = document.createElement("time");
-            var formattedDate = articleData.date.replace(/\//g,"-");
-            time.innerHTML = formattedDate;
-            time.setAttribute("datetime", formattedDate);
-            
-            article.appendChild(title);
-            article.appendChild(time);
-            
-            var numComments = articleData.commentcount;
-            if(numComments !== void 1) {
-                var comments = document.createElement("a");
-                if(numComments===1) {
-                    comments.innerHTML = numComments + " comment";
-                } else {
-                    comments.innerHTML = numComments + " comments";
-                }
-                comments.className = "comments";
-                comments.setAttribute("href", articleData.commenturl);
-                comments.setAttribute("target", "_blank");
-                article.appendChild(comments);
-            } else {
-                var nocomments = document.createElement("span");
-                nocomments.className = "comments";
-                nocomments.innerHTML = "&nbsp;"; //keep block formatting
-                article.appendChild(nocomments);
-            }
+    var rootRef = new Firebase('https://scrap-dev-news.firebaseio.com/');
 
-            var content = document.getElementById("content");
-            content.appendChild(article);
-        }
+    function displayLastUpdateDate(snapshot) {
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var date = new Date(snapshot.val().lastupdate);
+        var formattedDate = date.toDateString();
+        var formattedTime = date.toTimeString();
+        var updateField = document.getElementById("updatetime");
+        updateField.innerHTML = formattedDate.slice(4, 10) + " @ " + formattedTime.slice(0, 5);
     }
 
-}).then(function() {
-    // var h2 = document.getElementsByTagName("h2");
-    // document.body.removeChild(h2[0]);
-});
+    function removeLoadingMessage() {
+        var h2 = document.getElementsByTagName("h2");
+        document.body.removeChild(h2[0]);
+    }
+
+    function reverseSnapshot(snapshot) {
+        var snapshotArray = [];
+        snapshot.forEach(function(snap) {
+            snapshotArray.push(snap.val());
+        });
+        return snapshotArray.reverse();
+    }
+
+    function compare(a, b) {
+        if (a.millis < b.millis) return 1;
+        else if (a.millis > b.millis) return -1;
+        else return 0;
+    }
+
+    function fetchAll(snapshot) {
+        var dataArray = reverseSnapshot(snapshot);
+
+        var articles = [];
+
+        for (var i = 0, len = dataArray.length; i < len; i++) {
+            var site = dataArray[i];
+            for (var article in site) {
+                articles.push(site[article]);
+            }
+        }
+        
+        articles.sort(compare); // most recent data shows first
+        
+        for (var key in articles) {
+            if (articles.hasOwnProperty(key)) {
+                var articleData = articles[key];
+                var siteTag = articleData.tag;
+
+                var article = document.createElement("article");
+                article.innerHTML = '<svg class="left" width="54" height="54" viewbox="0 0 200 200"><use xlink:href="#sym-' + siteTag + '"></use></svg>';
+
+                var title = document.createElement("a");
+                title.className = "title";
+                title.innerHTML = articleData.title;
+                title.setAttribute("href", articleData.url);
+                title.setAttribute("target", "_blank");
+
+                var time = document.createElement("time");
+                var formattedDate = articleData.date.replace(/\//g, "-");
+                time.innerHTML = formattedDate;
+                time.setAttribute("datetime", formattedDate);
+
+                article.appendChild(title);
+                article.appendChild(time);
+
+                var numComments = articleData.commentcount;
+                if (numComments !== void 1) {
+                    var comments = document.createElement("a");
+                    if (numComments === 1) {
+                        comments.innerHTML = numComments + " comment";
+                    } else {
+                        comments.innerHTML = numComments + " comments";
+                    }
+                    comments.className = "comments";
+                    comments.setAttribute("href", articleData.commenturl);
+                    comments.setAttribute("target", "_blank");
+                    article.appendChild(comments);
+                } else {
+                    var nocomments = document.createElement("span");
+                    nocomments.className = "comments";
+                    nocomments.innerHTML = "&nbsp;"; //keep block formatting
+                    article.appendChild(nocomments);
+                }
+
+                var content = document.getElementById("content");
+                content.appendChild(article);
+            }
+        }
 
 
+    }
 
+    rootRef.child("settings").once("value", displayLastUpdateDate);
+    rootRef.child("sites").once("value").then(fetchAll).then(removeLoadingMessage);
+
+
+})();
 },{"firebase":2}],2:[function(require,module,exports){
 /*! @license Firebase v2.4.1
     License: https://www.firebase.com/terms/terms-of-service.html */
